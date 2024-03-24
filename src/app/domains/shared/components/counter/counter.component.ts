8ca594bdc994f27ddb9715f731739425c9ce8178
= <wagnersmith123@hotmail.com>
2024-03-24 16:54:40 -0500

    aprendiendo cómo se manejan los ciclos de vida en Angular

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required: true}) duration = 0
+  @Input ({required: true}) message = ""
+
+  constructor(){
+    //NO ASYNC
+    // before render
+    console.log("constructor")
+    console.log("-".repeat(10))
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    //before and during render
+    console.log("ngOnChanges")
+    console.log("-".repeat(10))
+    console.log(changes)
+  }
+
+  ngOnInit(){
+    //after render
+    //una vez
+    //async, then, subs
+    console.log("ngOnInit")
+    console.log("-".repeat(10))
+    console.log('duration =>', this.duration)
+    console.log('message =>', this.message)
+  }
+  ngAfterViewInit(){
+    //after render
+    //Hijos ya fueron renderizados
+    console.log("ngAfterViewInit")
+    console.log("-".repeat(10))
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestroy")
+    console.log("-".repeat(10))
+  }
+}
