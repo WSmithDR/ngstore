fcd9ecfed94e0880646bb86304ebfb0f92401cdf
= <wagnersmith123@hotmail.com>
2024-03-24 14:03:22 -0500

    aprendiendo a mapear y consumir informacion por mi cuenta e integrando el cocepto del output que es una manera de crear un event para enviar informacion al compoente padre por de algun componente hijo

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -10,5 +10,24 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  img = "https://picsum.photos/640/640?r="+Math.random()
+  createData(num:number){
+    let data = []
+    for(let i=1; i<=num; i++){
+      const j = Math.floor(Math.random() * (num - 1 + 1)) + 1
+      const item = {
+        id: j,
+        title: `Producto ${j}`,
+        price: Number(`${j}000`),
+        img: `https://picsum.photos/640/640?r=${j}`
+      }
+      data.push(item)
+    }
+    return data
+  }
+
+  products = this.createData(10)
+  fromChild(event:string){
+    console.log("Estamos en el padre!")
+    console.log(event)
+  }
 }
