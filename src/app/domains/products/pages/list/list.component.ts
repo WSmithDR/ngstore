22e9288db527c42f35bcf1e4f6800efe2232cb70
= <wagnersmith123@hotmail.com>
2024-03-24 13:20:28 -0500

    aprendiendo a integrar los componentes a la página y entender el concepto de los inputs

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  img = "https://picsum.photos/640/640?r="+Math.random()
 }
