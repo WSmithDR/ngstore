419e2a82242e1e12d17adc0c2df57657a60c87b2
= <wagnersmith123@hotmail.com>
2024-03-25 17:25:35 -0500

    Integrado la parte de calcular el precio total de los productos añadidos al carrito de compras

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
 
 @Component({
@@ -10,10 +10,22 @@ import { Product } from '../../models/product.model';
 })
 export class HeaderComponent {
   hideSideMenu = signal(true)
-
   @Input({required:true}) cart!: Product[]
+  total = signal(0)
+
 
   toogleSideMenu(){
     this.hideSideMenu.update(prevState => !prevState)
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    const cart = changes['cart']
+    if(cart){
+      this.total.set(this.calcTotal())
+    }
+  }
+
+  calcTotal(){
+    return this.cart.reduce((total,product)=> total + product.price, 0)
   }
 }
