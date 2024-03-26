c6da032513e4ed12d0d6330e6ec841745cc2b786
= <wagnersmith123@hotmail.com>
2024-03-26 18:09:51 -0500

    creado el compoenten productDetail y aplicando su enrutado dinámico, pero aun no consume la info dada por su parámetro

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 import { Product } from '../../../shared/models/product.model';
@@ -7,7 +8,7 @@ import { Product } from '../../../shared/models/product.model';
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, ReversePipe, TimeAgoPipe],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe, RouterLinkWithHref],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
