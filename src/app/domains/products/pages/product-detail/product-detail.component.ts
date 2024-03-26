7606c063e673e751ccee219d7ba239d5d0ed8e0b
= <wagnersmith123@hotmail.com>
2024-03-26 18:42:25 -0500

    avanzndo con el produtDetail. Falta que al clickear una de las imágnees esta se reemplace en el cover

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, inject, signal } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  @Input() prdId?: string
+  product = signal<Product | null>(null)
+  private productService = inject(ProductService)
+
+  ngOnInit(){
+    if(this.prdId){
+      this.productService.getOne(this.prdId)
+      .subscribe({
+        next: (product)=>{
+          this.product.set(product)
+        }
+      })
+    }
+  }
 
 }
