e111b6f944183c33c84a5f559e765eb489b11e15
= <wagnersmith123@hotmail.com>
2024-03-26 18:55:29 -0500

    Se consiguió hacer funcionar el boton de añadir al carrito del product detail

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -2,6 +2,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, inject, signal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -16,6 +17,7 @@ export class ProductDetailComponent {
   product = signal<Product | null>(null)
   cover = signal('')
   private productService = inject(ProductService)
+  private cartService = inject(CartService)
 
   ngOnInit(){
     if(this.prdId){
@@ -33,6 +35,13 @@ export class ProductDetailComponent {
 
   changeCover(newImg: string){
     this.cover.set(newImg)
+  }
+
+  addToCart(){
+    const product = this.product()
+    if(product){
+      this.cartService.addToCart(product)
+    }
   }
 
 }
