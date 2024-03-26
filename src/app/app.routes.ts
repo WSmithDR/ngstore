6c6d7323b43a626ec218c9e3be59e651252c6291
= <wagnersmith123@hotmail.com>
2024-03-26 17:54:05 -0500

    creado un layout para toda la webapp para facilitar el posicionamiento y estilizado del encabezado, cuerpo y pie de página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 import { ListComponent } from './domains/products/pages/list/list.component';
 
 export const routes: Routes = [
   {
-    path:"",
-    component:ListComponent
-  },
-  {
-    path:"about",
-    component: AboutComponent
+    path:'',
+    component: LayoutComponent,
+    children: [
+      {
+        path:"",
+        component:ListComponent
+      },
+      {
+        path:"about",
+        component: AboutComponent
+      },
+    ]
   },
   {
     path:'**',
